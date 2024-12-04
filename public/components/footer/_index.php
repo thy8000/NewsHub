1cad393bb8f3046a0fdca6e199dda0740e3abc35
thy8000 <thunaymoreiradesoares@gmail.com>
2024-12-03 23:57:38 -0300

    🚧 Home; Footer; Header

---
+++ public/components/footer/_index.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!defined('ABSPATH')) {
+   exit;
+}
+
+?>
+
+</main>
+
+<footer id="footer">
+   Footer
+</footer>
+
+<?php
+
+wp_footer();
+
+?>
+
+</body>
+
+</html>
---
+++ public/pages/home/_index.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('ABSPATH')) {
+   exit;
+}
+
+get_template_part('public/components/header/_index');
+
+?>
+
+<div>
+   Home
+</div>
+
+<?php
+
+get_template_part('public/components/footer/_index');
